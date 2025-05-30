cbfea9770593f961fb2ec7862d0abeb3d0795a3f
linyu <bigsuperangel@gmail.com>
2025-05-30 16:12:16 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // 关键配置：启用静态导出（解决 "Output directory not found"）
+  output: 'export',
+   // 静态导出相关配置
+  trailingSlash: true,            // 确保URL以/结尾
+    images: {
+    unoptimized: true,            // 禁用图片优化（静态站点不需要）
+  }
 };
 
 export default nextConfig;
